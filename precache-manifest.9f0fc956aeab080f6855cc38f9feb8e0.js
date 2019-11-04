45a6484b302090154065009e7a97ca093e13cd7b
Aishwarya <aishsahoo5@gmail.com>
2019-11-04 13:01:38 +0530

    Updates

---
+++ precache-manifest.9f0fc956aeab080f6855cc38f9feb8e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fce4e2d3d69208b74672b8926d76777",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "5e371a95f90924708193",
+    "url": "/monster-react/static/css/main.04a175a9.chunk.css"
+  },
+  {
+    "revision": "a90d5c1bac724ed75b90",
+    "url": "/monster-react/static/js/2.7a7e1856.chunk.js"
+  },
+  {
+    "revision": "5e371a95f90924708193",
+    "url": "/monster-react/static/js/main.c1a4efda.chunk.js"
+  },
+  {
+    "revision": "1a5e2f0955aa6bcc14a4",
+    "url": "/monster-react/static/js/runtime-main.9bb363cb.js"
+  }
+]);
